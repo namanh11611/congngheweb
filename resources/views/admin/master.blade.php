f48f6f220dc350c18427b46e2e725d83b39e3347
DESKTOP-3RTQHE4\KhacTu <nguyenkhactu1996@gmail.com>
2017-05-10 23:16:15 +0700

    4 commit

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -65,7 +65,7 @@
                     </a>
                     <ul class="dropdown-menu dropdown-user">
                     
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i>UserName</a>
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i>Admin</a>
                         </li>
                         <li class="divider"></li>
                         <li><a href="{{
