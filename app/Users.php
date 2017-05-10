217de22eadaa4492841e86b8cef3e0ced07faca6
DESKTOP-3RTQHE4\KhacTu <nguyenkhactu1996@gmail.com>
2017-05-10 17:08:47 +0700

    fix Users.php

--- app/Users.php
+++ app/Users.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\CanResetPassword;
-use Illuminate\Auth\Passwords\CanResetPassword;
 
 class Users extends Model
 {
